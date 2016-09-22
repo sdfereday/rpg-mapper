c099b41a5194de922479d865139787ec5f8c18cb
Sam Fereday <sam.fereday@shoutdigital.com>
2016-09-22 10:56:26 +0100

    initial

---
+++ require.conf.js
@@ -0,0 +1,11 @@
+var require = {
+    paths: {
+        "ko": "bower_components/knockout/dist/knockout",
+        "ROT": "bower_components/rot.js/rot"
+    },
+    shim: {
+    	"ROT": {
+    		exports: "ROT"
+    	}
+    }
+};
