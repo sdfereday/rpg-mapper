49172c2f9bc593dfad0b6d83ad6eecd803debb2e
Sam Fereday <sfereday@gmail.com>
2018-02-24 18:17:18 +0000

    - Remove bower - Install react and all dependencies for build

--- App.js
+++
@@ -1,9 +0,0 @@
-import AppComponent from 'src/Root/AppComponent';
-
-ReactDOM.render(
-    <AppComponent
-        appTitle="React Mapper"
-        version="0.1a"
-    />,
-    document.getElementById('container')
-);
