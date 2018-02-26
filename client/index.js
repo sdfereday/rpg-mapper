8cc1f35e8b940bb93834c04a2f5c6094c0312939
Sam Fereday <sfereday@gmail.com>
2018-02-26 21:16:51 +0000

    Remove old directory and bump version

--- client/index.js
+++ client/index.js
@@ -5,6 +5,6 @@ import App from './src/Root/Container/App';
 import './scss/core.scss';
 
 ReactDOM.render(
-     <App appTitle="React Mapper" version="0.1a" />,
+     <App appTitle="React Mapper" version="1.0" />,
      document.getElementById('container')
 );
